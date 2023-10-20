e99f0871da43bf08cf261ae01c2f3c9c1ed12f61
Shashank Rao <shashank.rao@hevodata.com>
2023-10-20 08:32:55 +0530

    Calculator

--- grpc-client/src/main/java/io/shashank/grpc/client/GRPCClient.java
+++ grpc-client/src/main/java/io/shashank/grpc/client/GRPCClient.java
@@ -3,47 +3,91 @@ package io.shashank.grpc.client;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import io.shashank.grpc.service.calculator.CalculatorServiceGrpc;
+import io.shashank.grpc.service.calculator.CalculatorServiceGrpc.CalculatorServiceStub;
+import io.shashank.grpc.service.calculator.Op;
+import io.shashank.grpc.service.calculator.Request;
+import io.shashank.grpc.service.calculator.Response;
 import io.shashank.grpc.service.hello.Hello;
 import io.shashank.grpc.service.hello.HelloResponse;
 import io.shashank.grpc.service.hello.HelloServiceGrpc;
 import io.shashank.grpc.service.hello.HelloServiceGrpc.HelloServiceBlockingStub;
 import io.shashank.grpc.service.hello.HelloServiceGrpc.HelloServiceFutureStub;
+import java.util.Random;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GRPCClient {
 
   public static void main(String[] args) throws InterruptedException {
-    helloSync();
-    helloAsync();
+    streamingClientCalculator();
+//    helloSync();
+//    helloAsync();
     Thread.sleep(99999999999999999L);
+  }
+
+  private static void streamingClientCalculator() {
+    log.info("Sending stream of calculator requests");
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext()
+        .build();
+    CalculatorServiceStub service = CalculatorServiceGrpc.newStub(channel);
+    StreamObserver<Request> request = service.calculate(
+        new StreamObserver<>() {
+          @Override
+          public void onNext(Response response) {
+            log.info("Calculated number: {}", response.getResult());
+          }
+
+          @Override
+          public void onError(Throwable throwable) {
+            log.error("Server threw an error", throwable);
+            channel.shutdown();
+          }
+
+          @Override
+          public void onCompleted() {
+            channel.shutdown();
+          }
+        });
+
+    Op[] ops = new Op[]{Op.A, Op.S, Op.M, Op.D};
+    Random randomizer = new Random();
+    for (int i = 0; i < 10; i++) {
+      Op op = ops[randomizer.nextInt(ops.length)];
+      double num = randomizer.nextInt() * 1.0d;
+      log.info("{} {}", op, num);
+      request.onNext(Request.newBuilder().setOp(op).setNum(num).build());
+    }
+    log.info("All calculation requests complete");
+    request.onCompleted();
   }
 
   private static void helloSync() {
     log.info("Sending a hello request");
-    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext()
+        .build();
     HelloServiceBlockingStub helloService = HelloServiceGrpc.newBlockingStub(channel);
     HelloResponse response = helloService.hello(
         Hello.newBuilder().setName("shashank").build());
-    log.info("got response: "+ response.getGreeting());
+    log.info("got response: " + response.getGreeting());
     channel.shutdown();
   }
 
   private static void helloAsync() {
     log.info("Sending a hello async request");
-    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext().build();
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext()
+        .build();
     HelloServiceFutureStub helloService = HelloServiceGrpc.newFutureStub(channel);
     ListenableFuture<HelloResponse> response = helloService.hello(
         Hello.newBuilder().setName("shashank").build());
     Futures.addCallback(response, new FutureCallback<>() {
       @Override
       public void onSuccess(HelloResponse result) {
-        log.info("got async response: "+ result.getGreeting());
+        log.info("got async response: " + result.getGreeting());
       }
 
       @Override
--- grpc-server/src/main/java/io/shashank/grpc/server/GRPCServer.java
+++ grpc-server/src/main/java/io/shashank/grpc/server/GRPCServer.java
@@ -2,6 +2,7 @@ package io.shashank.grpc.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.shashank.grpc.service.calculator.CalculatorService;
 import io.shashank.grpc.service.hello.HelloService;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 public class GRPCServer {
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    log.info("Starting GRPC server");
-    Server server = ServerBuilder.forPort(8080).addService(new HelloService()).build();
+    log.info("Starting GRPC Server!");
+    Server server = ServerBuilder.forPort(8080).addService(new HelloService())
+        .addService(new CalculatorService()).build();
     Runtime.getRuntime().addShutdownHook(new Thread(() -> log.info("GRPC Server going down")));
     server.start();
     log.info("Started");
